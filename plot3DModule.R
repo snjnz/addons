a0c89d085063009a13fe1207ebb9daa0ec994a29
Tom Elliott <tomelliottnz@gmail.com>
2020-04-15 15:40:52 +1200

    Add meta data to 3d plot addon

--- plot3DModule.R
+++ plot3DModule.R
@@ -1,3 +1,7 @@
+#' @name 3D Plotting
+#' @version 0.0.1
+#' @author Tom Elliott
+#' @desc Explore three variables in a three-dimensional plot.
 Plot3DModule <- setRefClass(
     "3D Scatter Plot",
     contains = "CustomModule",
@@ -16,8 +20,8 @@ Plot3DModule <- setRefClass(
     ),
     methods = list(
         initialize = function(gui, name) {
-            callSuper(gui, 
-                name = name, 
+            callSuper(gui,
+                name = name,
                 embedded = TRUE
             )
             initFields(
@@ -38,9 +42,9 @@ Plot3DModule <- setRefClass(
             ## The main code for your module goes here,
             ## inside a top-level container called "mainGrp"
             mainGrp$set_borderwidth(5)
-            
+
             ## --- variable list
-            
+
 
             ## --- chosen variable boxes
             tbl <- glayout()
@@ -51,7 +55,7 @@ Plot3DModule <- setRefClass(
 
             lbl <- glabel("x variable :")
             xvarDrop <- gcombobox(
-                c("Select X Variable", numvars), 
+                c("Select X Variable", numvars),
                 selected = 1,
                 handler = function(h, ...) {
                     if (svalue(h$obj, index = TRUE) == 1) var_x <<- ""
@@ -65,7 +69,7 @@ Plot3DModule <- setRefClass(
 
             lbl <- glabel("y variable :")
             yvarDrop <- gcombobox(
-                c("Select Y Variable", numvars), 
+                c("Select Y Variable", numvars),
                 selected = 1,
                 handler = function(h, ...) {
                     if (svalue(h$obj, index = TRUE) == 1) var_y <<- ""
@@ -79,7 +83,7 @@ Plot3DModule <- setRefClass(
 
             lbl <- glabel("z variable :")
             zvarDrop <- gcombobox(
-                c("Select Z Variable", numvars), 
+                c("Select Z Variable", numvars),
                 selected = 1,
                 handler = function(h, ...) {
                     if (svalue(h$obj, index = TRUE) == 1) var_z <<- ""
@@ -96,7 +100,7 @@ Plot3DModule <- setRefClass(
                 selected = 1,
                 handler = function(h, ...) {
                     if (svalue(h$obj, index = TRUE) == 1) var_group <<- ""
-                    else var_group <<- svalue(h$obj)        
+                    else var_group <<- svalue(h$obj)
                     updatePlot()
                 }
             )
@@ -131,7 +135,7 @@ Plot3DModule <- setRefClass(
 
             ## Render in 3D (using rgl)
             if (requireNamespace('plot3Drgl', quietly = TRUE)) {
-                btn <- gbutton("Open interactive plot", 
+                btn <- gbutton("Open interactive plot",
                     container = mainGrp,
                     handler = function(h, ...) {
                         updatePlot(interactive = TRUE)
@@ -140,7 +144,7 @@ Plot3DModule <- setRefClass(
             }
 
             ## The UI provides a way for users to change values of "fields"
-            
+
             ## if you can do it without any user input, draw a basic plot
             updatePlot()
         },
@@ -169,14 +173,14 @@ Plot3DModule <- setRefClass(
             }
 
             if (!interactive) {
-                plot3D::scatter3D(x, y, z, 
+                plot3D::scatter3D(x, y, z,
                     colvar = colvar, col = col, colkey = FALSE,
                     theta = theta, phi = phi,
                     xlab = var_x, ylab = var_y, zlab = var_z,
                     bty = "b2", pch = 19
                 )
                 if (var_group != "") {
-                    legend("right", 
+                    legend("right",
                         levels(data[[var_group]]),
                         col = col,
                         pch = 19,
