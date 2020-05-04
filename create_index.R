20bec2baa52f075e327921ff7e6635a4d275f4cf
Tom Elliott <tomelliottnz@gmail.com>
2020-05-04 15:58:11 +1200

    Comment out so DemoModule displays (until we have more modules)

--- create_index.R
+++ create_index.R
@@ -5,7 +5,7 @@
 ## This comes from iNZightModules :
 getModules <- function(dir) {
     mods <- list.files(dir, pattern = "*.R", full.names = TRUE)
-    mods <- mods[!grepl("DemoModule", mods)]
+    # mods <- mods[!grepl("DemoModule", mods)]
     mod_list <- lapply(mods, getmodule)
     mod_list <- mod_list[!sapply(mod_list, is.null)]
     names(mod_list) <- sapply(mod_list, function(x) x$name)
