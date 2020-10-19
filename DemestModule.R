4e4462df141b7d54d7d343f2bc65a34debd95cb8
Tom Elliott <tomelliottnz@gmail.com>
2020-10-19 15:32:03 +1300

    Having trouble collapsing dimensions

--- DemestModule.R
+++ DemestModule.R
@@ -13,7 +13,9 @@ DemestModule <- setRefClass(
         exposure_var = "ANY",
         tab_data = "ANY",
         exposure = "ANY",
-        used_vars = "ANY"
+        used_vars = "ANY",
+        response_lhfun = "ANY",
+        response_lhfmla = "ANY"
     ),
     methods = list(
         initialize = function(gui, name) {
@@ -47,33 +49,33 @@ DemestModule <- setRefClass(
             g_response$set_borderwidth(5)
             tbl_response <- glayout(container = g_response,
                 expand = TRUE)
-            ii <- 1
+            ii <- 1L
 
             varnames <- colnames(raw_data)
             vartypes <- sapply(raw_data, iNZightTools::vartype)
             response_var <<- gcombobox(varnames,
-                selected = 0,
+                selected = 0L,
                 handler = function(h, ...) {
                     set_vars()
                     visible(response_type) <<- TRUE
                 }
             )
             lbl <- glabel("Response variable: ")
-            tbl_response[ii, 1, anchor = c(1, 0), expand = TRUE] <- lbl
+            tbl_response[ii, 1L, anchor = c(1, 0), expand = TRUE] <- lbl
             tbl_response[ii, 2:3, expand = TRUE] <- response_var
-            ii <- ii + 1
+            ii <- ii + 1L
 
             exposure_var <<- gcombobox(c("None", varnames),
                 selected = 1L,
                 handler = function(h, ...) set_vars()
             )
             lbl <- glabel("Exposure variable: ")
-            tbl_response[ii, 1, anchor = c(1, 0), expand = TRUE] <- lbl
+            tbl_response[ii, 1L, anchor = c(1, 0), expand = TRUE] <- lbl
             tbl_response[ii, 2:3, expand = TRUE] <- exposure_var
-            ii <- ii + 1
+            ii <- ii + 1L
 
             response_type <<- gcombobox(c("Counts", "Values"),
-                selected = 0,
+                selected = 0L,
                 handler = function(h, ...) {
                     set_vars()
                     visible(g_vars) <- TRUE
@@ -82,9 +84,9 @@ DemestModule <- setRefClass(
             )
             visible(response_type) <<- FALSE
             lbl <- glabel("Response type: ")
-            tbl_response[ii, 1, anchor = c(1, 0), expand = TRUE] <- lbl
+            tbl_response[ii, 1L, anchor = c(1, 0), expand = TRUE] <- lbl
             tbl_response[ii, 2:3, expand = TRUE] <- response_type
-            ii <- ii + 1
+            ii <- ii + 1L
 
             ### -------------------------------------- Variable info
             g_vars <- gexpandgroup("Variable information",
@@ -96,15 +98,48 @@ DemestModule <- setRefClass(
             g_vars$set_borderwidth(5)
             tbl_vars <- glayout(container = g_vars,
                 expand = TRUE)
-            ii <- 1
+            ii <- 1L
 
             used_vars <<- gtable(varnames, multiple = TRUE)
             addHandlerSelectionChanged(used_vars,
-                handler = function(h, ...) updatePlot()
+                handler = function(h, ...) set_vars()
             )
-            tbl_vars[ii, 3, expand = TRUE] <- used_vars
+            tbl_vars[ii, 3L, expand = TRUE] <- used_vars
             size(used_vars) <<- c(-1, 200)
-            ii <- ii + 1
+            ii <- ii + 1L
+
+            vars_ok_btn <<- gbutton("Continue",
+                handler = function(h, ...) {
+                    visible(g_vars) <- FALSE
+                    visible(g_likelihood) <- TRUE
+                }
+            )
+            tbl_vars[ii, 3L, expand = TRUE] <- vars_ok_btn
+            ii <- ii + 1L
+
+            ### -------------------------------------- Variable info
+            g_likelihood <- gexpandgroup("Model likelihood",
+                container = mainGrp
+            )
+            visible(g_likelihood) <- FALSE
+            font(g_likelihood) <- list(weight = "bold")
+            g_likelihood$set_borderwidth(5)
+
+            tbl_likelihood <- glayout(container = g_likelihood,
+                expand = TRUE)
+            ii <- 1L
+
+            # Values: normal
+            # Counts: Binomial, Poisson
+            response_lhfun <<- gcombobox("Select response variable")
+            response_lhfmla <<- gedit("")
+
+            tbl_likelihood[ii, 1L, expand = TRUE, anchor = c(1, 0)] <- glabel("Response type :")
+            tbl_likelihood[ii, 2:3, expand = TRUE] <- response_lhfun
+            ii <- ii + 1L
+
+            tbl_likelihood[ii, 1L, expand = TRUE, anchor = c(1, 0)] <- glabel("Formula :")
+            tbl_likelihood[ii, 2:3, expand = TRUE] <- response_lhfmla
 
 
         },
@@ -126,6 +161,7 @@ DemestModule <- setRefClass(
                 used_vars$set_items(vars)
                 used_vars$set_value(chosen_vars)
             } else {
+                chosen_vars <- vars
                 used_vars$set_items(vars)
             }
             unblockHandlers(used_vars)
@@ -136,10 +172,20 @@ DemestModule <- setRefClass(
             tab_data <<- make_dem_array(raw_data, var_y, vars, r_type, d_types, d_scales)
             exposure <<- make_dem_array(raw_data, var_n, vars, "Counts", d_types, d_scales)
 
+            # set likelihood info
+            response_lhfun$set_items(
+                if (r_type == "Values") "Normal" else c("Poisson", "Binomial")
+            )
+            response_lhfun$set_index(1L)
+            response_lhfmla$set_value(
+                sprintf("(%s)^2", paste(chosen_vars, collapse = " + "))
+            )
+
             updatePlot()
         },
         make_dem_array = function(data, y, x, type, dimtypes, dimscales) {
             if (is.null(y)) return(NULL)
+            d <- raw_data
             arr <- tapply(raw_data[[y]], raw_data[x], c)
             f <- switch(type,
                 Counts = dembase::Counts,
@@ -180,19 +226,32 @@ DemestModule <- setRefClass(
                     etmp <- dembase::collapseDimension(exposure, dimension = cvar)
                 }
             }
+            if (length(vars) > 4) {
+                # collapse down dimensions; requires counts
+                cvar <- vars[vars %notin% svalue(used_vars)[1:4]]
+                vars <- vars[vars %in% svalue(used_vars)[1:4]]
+                if (svalue(response_type) == "Counts") {
+                    tmp <- dembase::collapseDimension(tmp, dimension = cvar)
+                } else if (!is.null(exposure)) {
+                    tmp <- dembase::collapseDimension(
+                        tmp,
+                        dimension = cvar,
+                        weights = exposure
+                    )
+                    etmp <- dembase::collapseDimension(exposure, dimension = cvar)
+                } else {
+                    stop("Please specify exposure variable")
+                }
+            }
             if (!is.null(exposure)) {
                 tmp <- tmp / etmp
-            }
-            if (length(ovar) > 3) {
-                # collapse down dimensions; requires counts
-                stop("Cannot handle more than 4 dims yet")
             }
 
             df <- as.data.frame(tmp,
                 direction = "long",
                 midpoints = tvar)
 
-            print(vars)
+            # print(vars)
             p <- ggplot2::ggplot(df,
                 ggplot2::aes_(
                     as.name(vars[1]),
